b00529909ae644b438ad7792de21c61e30b9bc88
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-05-03 19:02:51 +0530

    Fundamentals

--- sample.py
+++ sample.py
@@ -1,8 +1,9 @@
-#1 Import Libraries
+# 1 Import Libraries
 import sys
 from PyQt6 import QtGui, QtCore, QtWidgets
 
-#2 Create One Class
+
+# 2 Create One Class
 class ApplicationForm:
     # 3 set dialog
     def __init__(self, form):
@@ -13,20 +14,50 @@ class ApplicationForm:
         self.dialog.setEnabled(True)
         self.dialog.setWindowTitle("Ajith")
 
-#Execute Application
+        # Give the introduction about widgets on constructor itself
+        self.lb_title = None
+        self.PushB = None
+
+    # 4  Set Widgets
+    def setup_Widgets(self):
+
+        # Label
+        self.lb_title = QtWidgets.QLabel(self.dialog)
+        self.lb_title.setObjectName("Label")
+        self.lb_title.setGeometry(125, 90, 130, 30)
+        self.lb_title.setText('Hello !')
+
+        # Push Button
+        self.PushB = QtWidgets.QPushButton(self.dialog)
+        self.PushB.setObjectName("Button")
+        self.PushB.setGeometry(120, 120, 150, 30)
+        self.PushB.setText("Welcome")
+
+        # Set Signal
+        self.PushB.clicked.connect(self.Event)
+
+        # Set Slot
+        QtCore.QMetaObject.connectSlotsByName(self.dialog)
+
+    # 5 set Events
+    def Event(self):
+        # To change the text of the Label use 'lb_title' function
+        self.lb_title.setText("Iam Learning PyCharm")
+
+
+# Execute Application
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     App = QtWidgets.QWidget()
 
-    #Give the form class
+    # Give the form class
     userI = ApplicationForm(App)
 
-    #Show Form
+    # Initialize the Widget
+    userI.setup_Widgets()
+
+    # Show Form
     App.show()
 
-    #Close the Form
+    # Close the Form
     sys.exit(app.exec())
-
-
-
-
