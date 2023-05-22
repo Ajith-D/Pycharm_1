263e12ead6370911863a584fd9755d6c6f7b5b3c
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-05-22 21:35:08 +0530

    Fundamentals

--- 4 QComboBox.py
+++ 4 QComboBox.py
@@ -65,10 +65,103 @@ out_y = (pushb_y + pushb_height) + 20
 
 class AppForm:
     def __init__(self, form):
-        form = QWidget()
+        # form = QWidget()
         self.dialog = form
         self.dialog.setGeometry(100, 100, dlg_width, dlg_height)
+        self.dialog.setWindowTitle('Sign-up Form')
+
+        self.title = None
+        self.name = None
+        self.line1 = None
+        self.age = None
+        self.line2 = None
+        self.gen = None
+        self.com = None
+        self.pushb = None
+        self.out = None
+
+    def Widgets(self):
+        self.title = QLabel(self.dialog)
+        self.name = QLabel(self.dialog)
+        self.line1 = QLineEdit(self.dialog)
+        self.age = QLabel(self.dialog)
+        self.line2 = QLineEdit(self.dialog)
+        self.gen = QLabel(self.dialog)
+        self.com = QComboBox(self.dialog)
+        self.pushb = QPushButton(self.dialog)
+        self.out = QLabel(self.dialog)
+
+        # Title
+        self.title.setGeometry(title_x, title_y, title_width, title_height)
+        self.title.setText('AATOMZ')
+        self.title.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Name
+        self.name.setGeometry(name_x, name_y, name_width, name_height)
+        self.name.setText('NAME: ')
+        self.name.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Line1
+        self.line1.setGeometry(line1_x, line1_y, line1_width, line1_height)
+        self.line1.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.line1.setStyleSheet('Background-color : lightgrey')
+
+        # Age
+        self.age.setText('AGE: ')
+        self.age.setGeometry(age_x, age_y, age_width, age_height)
+        self.age.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Line 2
+        self.line2.setGeometry(line2_x, line2_y, line2_width, line2_height)
+        self.line2.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.line2.setStyleSheet('Background-color : lightgrey')
+
+        # Gender
+        self.gen.setGeometry(gen_x, gen_y, gen_width, gen_height)
+        self.gen.setText('GENDER: ')
+        self.gen.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        # Combo Box
+        self.com.setGeometry(combo_x, combo_y, combo_width, combo_height)
+        self.com.setObjectName('COMBO BOX')
+        self.com.addItem('MALE')
+        self.com.addItem('FEMALE')
+
+        # Push Button
+        self.pushb.setGeometry(pushb_x, pushb_y, pushb_width, pushb_height)
+        self.pushb.setText('SIGN UP')
+        self.pushb.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Output
+        self.out.setGeometry(out_x, out_y, out_width, out_height)
+        self.out.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        # Set Signal and slot
+        self.pushb.clicked.connect(self.Events)
+        QMetaObject.connectSlotsByName(self.dialog)
+
+    def Events(self):
+        if self.gen.currentText() == 'MALE':
+            self.out.setText(f'''Welcome Sir!\n{self.line1.text()}
+            Successfully Signed Up''')
+
+        else:
+            self.out.setText(f'''Welcome Madam!\n{self.line1.text()}
+            Successfully Signed Up''')
 
 
+if __name__ == "__main_":
+    app = QApplication(sys.argv)
+    Form = QWidget()
 
+    # Give the Form class
+    user = AppForm(Form)
 
+    # Initialize the Widget
+    user.Widgets()
+
+    # Form Show
+    Form.show()
+
+    # Close
+    sys.exit(app.exec())
