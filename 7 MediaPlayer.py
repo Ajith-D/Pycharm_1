888ea315ac483879712e266a6b1fa3dd61bfc37b
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-05-29 09:38:55 +0530

    Fundamentals

--- 7 MediaPlayer.py
+++ 7 MediaPlayer.py
@@ -24,11 +24,20 @@ song_y = (title_height + title_y) + 10
 
 line1_width = 280
 line1_height = 30
-line1_x = (song_x + song_width) + 20
-line1_y = title_y - 5
+line1_x = (song_x + song_width) + 10
+line1_y = song_y - 5
 
 # Button Geometry
 pushb_width = 100
 pushb_height = 40
 pushb_x = (line1_x + line1_width) + 10
 pushb_y = song_y - 12
+
+# 3 Create Class
+class AppForm:
+    def __init__(self, form):
+        # form = QtWidgets.QWidget()
+        self.dialog = form
+        self.dialog.setGeometry(100, 200, dlg_width, dlg_height)
+        self.dialog.setWindowTitle('Media Player')
+
