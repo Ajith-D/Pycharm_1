3a3742a1e971f6bb221187c90f738e9734647b30
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-06-02 10:49:40 +0530

    Fundamentals

---
+++ vBoxLayout.py
@@ -0,0 +1,68 @@
+# 1 Import Libraries
+import sys
+from PyQt6.QtCore import *
+from PyQt6.QtWidgets import *
+
+# 2 Set Constant Values
+# Dialog Geometry
+dlg_width = 700
+dlg_height = 200
+
+# 3 Set class
+class AppForm:
+    def __init__(self, form):
+        #form = QWidget()
+        self.dialog = form
+        self.dialog.setGeometry(100, 100, dlg_width, dlg_height)
+        self.dialog.setWindowTitle('Button')
+        self.dialog.setStyleSheet('background-color : #336666')
+
+        #Set Field
+        self.pb_left = None
+        self.pb_right = None
+        self.pb_center = None
+        self.placement = None
+        self.layout = None
+
+    # 4 Create Widgets
+    def Widgets(self):
+        self.pb_left = QPushButton(self.dialog)
+        self.pb_left.setText('LEFT')
+        self.pb_left.setStyleSheet('color: White; background-color : Black')
+
+        self.pb_center = QPushButton(self.dialog)
+        self.pb_center.setText('CENTER')
+        self.pb_center.setStyleSheet('color: White; background-color : Black')
+
+        self.pb_right = QPushButton(self.dialog)
+        self.pb_right.setText('RIGHT')
+        self.pb_right.setStyleSheet('color: White; background-color : Black')
+
+        self.placement = QWidget(self.dialog)
+        self.layout = QVBoxLayout(self.dialog)
+        self.placement.setGeometry(0, 0, 300, 100)
+
+        # Uniform Sizing for all Left, right, center
+        self.layout.addWidget(self.pb_left)
+        self.layout.addWidget(self.pb_center)
+        self.layout.addWidget(self.pb_right)
+        self.dialog.setLayout(self.layout)
+
+
+
+# 5 Execute Application
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Form = QWidget()
+
+    # Give the form class
+    userI = AppForm(Form)
+
+    # Initialize the Widget
+    userI.Widgets()
+
+    # Show Form
+    Form.show()
+
+    # Close the Form
+    sys.exit(app.exec())
