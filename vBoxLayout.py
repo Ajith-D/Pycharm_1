830b86002123ba753191dd4928de8fce13fa8722
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-06-02 10:58:52 +0530

    Fundamentals

--- vBoxLayout.py
+++ vBoxLayout.py
@@ -5,7 +5,7 @@ from PyQt6.QtWidgets import *
 
 # 2 Set Constant Values
 # Dialog Geometry
-dlg_width = 700
+dlg_width = 400
 dlg_height = 200
 
 # 3 Set class
@@ -18,34 +18,34 @@ class AppForm:
         self.dialog.setStyleSheet('background-color : #336666')
 
         #Set Field
-        self.pb_left = None
-        self.pb_right = None
+        self.pb_top = None
+        self.pb_bottom = None
         self.pb_center = None
         self.placement = None
         self.layout = None
 
     # 4 Create Widgets
     def Widgets(self):
-        self.pb_left = QPushButton(self.dialog)
-        self.pb_left.setText('LEFT')
-        self.pb_left.setStyleSheet('color: White; background-color : Black')
+        self.pb_top = QPushButton(self.dialog)
+        self.pb_top.setText('TOP')
+        self.pb_top.setStyleSheet('color: White; background-color : Black')
 
         self.pb_center = QPushButton(self.dialog)
         self.pb_center.setText('CENTER')
         self.pb_center.setStyleSheet('color: White; background-color : Black')
 
-        self.pb_right = QPushButton(self.dialog)
-        self.pb_right.setText('RIGHT')
-        self.pb_right.setStyleSheet('color: White; background-color : Black')
+        self.pb_bottom = QPushButton(self.dialog)
+        self.pb_bottom.setText('BOTTOM')
+        self.pb_bottom.setStyleSheet('color: White; background-color : Black')
 
         self.placement = QWidget(self.dialog)
         self.layout = QVBoxLayout(self.dialog)
         self.placement.setGeometry(0, 0, 300, 100)
 
         # Uniform Sizing for all Left, right, center
-        self.layout.addWidget(self.pb_left)
+        self.layout.addWidget(self.pb_top)
         self.layout.addWidget(self.pb_center)
-        self.layout.addWidget(self.pb_right)
+        self.layout.addWidget(self.pb_bottom)
         self.dialog.setLayout(self.layout)
 
 
