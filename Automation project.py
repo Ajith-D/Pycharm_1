3fa785c7c30733cbcb247b9b10c167168f84b017
Ajith-D <125276212+Ajith-D@users.noreply.github.com>
2023-08-04 15:09:13 +0530

    Fundamentals

--- Automation project.py
+++ Automation project.py
@@ -72,12 +72,13 @@ if __name__ == '__main__':
             note_path = r"C:\Program Files\WindowsApps\Microsoft.WindowsNotepad_11.2305.18.0_x64__8wekyb3d8bbwe\Notepad\Notepad.exe"
             os.startfile(note_path)
 
+        elif 'tell me the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Hey Ajith, the time is {strTime}")
+
         elif 'open terminal' in query:
             t_path = r"C:\Users\User\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\System Tools\Command Prompt.lnk"
             os.startfile(t_path)
-
-        elif 'open youtube' in query:
-            webbrowser.open("https://www.youtube.com/")
 
         elif 'open Great Learning' in query:
             webbrowser.open("https://www.mygreatlearning.com/academy")
@@ -85,8 +86,23 @@ if __name__ == '__main__':
         elif 'open google' in query:
             webbrowser.open("https://www.google.com/")
 
-        elif 'tell me the time' in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Hey Ajith, the time is {strTime}")
+        elif 'open great learning youtube channels' in query:
+            webbrowser.open("https://www.youtube.com/user/beaconelearning")
+
+        elif 'open Linkedin' in query:
+            webbrowser.open("https://www.linkedin.com/feed/")
+
+        elif 'email to other friend' in query:
+            try:
+                speak("What should I send ?")
+                content = take_command()
+                to = "ajith.d2007@gmail.com"
+                sendMail(to, content)
+                speak("your email has benn sent successfully")
+
+            except Exception as ex:
+                print(ex)
+                speak('Unable to send the mail')
+
 
 
