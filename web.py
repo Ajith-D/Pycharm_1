bdf00b79debe594dd28d1b3149218baf6625c083
AJITH D <ajith.d2007@gmail.com>
2023-08-28 14:47:30 +0530

    Fundamentals

--- web.py
+++ web.py
@@ -1,5 +1,5 @@
 from flask import*
-
+import requests
 app = Flask(__name__)
 
 # By using route we can visit any pages without going to the homepage
@@ -10,6 +10,15 @@ def home():
 @app.route('/helpline')
 def help():
     return render_template('help.html')
+
+@app.route('/track', methods=['POST'])
+def track():
+    if request.method == 'POST':
+        url = request.form['user_url']
+        resp = requests.get(url)
+        redirect_url = resp.history
+        return render_template('track.html', endurl=resp, redirect=redirect_url)
+
 
 if __name__ == '__main__':
     # Debug is used to save automatically
