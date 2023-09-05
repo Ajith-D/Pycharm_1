d2b4c1e7537231ec817aae71ffbfc518c4c12b1c
AJITH D <ajith.d2007@gmail.com>
2023-09-05 11:19:59 +0530

    Fundamentals

---
+++ Practice/Prime number.py
@@ -0,0 +1,10 @@
+l = int(input('Enter: '))
+h = int(input('Enter: '))
+
+for num in range(l, h + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
