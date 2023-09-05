b548c0d069c2726d287df885010b213455ac1126
AJITH D <ajith.d2007@gmail.com>
2023-09-05 10:46:59 +0530

    Fundamentals

---
+++ Practice/Set Function.py
@@ -0,0 +1,17 @@
+tamil = ['Ajith', 'Bala', 'kavin', 'Anbu']
+english = ['Ben', 'Ajith', 'kavin','sathish']
+maths = ['Ajith', 'Anbu', 'pavithra', 'Ben']
+science = ['Aravind', 'Ajith', 'Geetha', 'sathish']
+social = ['kavin', 'pavithra', 'Ajith', 'Geetha']
+
+sTamil = set(tamil)
+sEnglish = set(english)
+smaths = set(maths)
+sScience = set(science)
+sSocial = set(social)
+
+# Union - To merge
+print(sTamil.union(smaths, sEnglish))
+# Intersection - To find common on the list
+print(sTamil.intersection(sSocial, sScience))
+
