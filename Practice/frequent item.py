82fbb995864d02edd0f23aa77cc406be8c281f92
AJITH D <ajith.d2007@gmail.com>
2023-09-22 12:21:18 +0530

    Fundamentals

---
+++ Practice/frequent item.py
@@ -0,0 +1,2 @@
+num = [1, 2, 3, 1, 4, 6, 8, 2]
+print(max(set(num), key=num.count))
--- Practice/pyramid.py
+++ Practice/pyramid.py
@@ -2,5 +2,5 @@ row = int(input('Enter: '))
 
 for i in range(row):
     for j in range(i + 1):
-        print("A", end="")
-    print("\n")
+        print('A', end="")
+    print('\n')
