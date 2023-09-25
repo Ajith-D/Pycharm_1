a8a0c35bb13cf10956c3f10074c1326e7dd1f0f8
AJITH D <ajith.d2007@gmail.com>
2023-09-25 10:11:23 +0530

    Fundamentals

--- Practice/Regex.py
+++ Practice/Regex.py
@@ -25,6 +25,8 @@ print(re.findall('234$', para))
 
 # Zero or more occurrence *
 print(re.findall('Hi.*g', para))
+# to find until specific word
+print(re.findall('H.*20', para))
 
 # Specified number {}
 print(re.findall('A.{3}h', para))
