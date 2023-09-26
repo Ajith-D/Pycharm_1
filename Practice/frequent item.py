a49d557ba40c2e6e4048dc42c73199b1fb1df2c0
AJITH D <ajith.d2007@gmail.com>
2023-09-26 15:50:58 +0530

    Fundamentals

--- Practice/frequent item.py
+++ Practice/frequent item.py
@@ -1,3 +1,3 @@
-list = [1, 2, 3, 4, 5, 1, 6, 1]
+list = [1, 2, 3, 2, 4, 5, 6, 7, 8, 1, 2]
 
 print(max(set(list), key=list.count))
--- Practice/largest number.py
+++ Practice/largest number.py
@@ -1,15 +1,14 @@
-# Find top 5 largest numbers
-list = [1, 2, 3, 0, 3, 0, 7, 9]
+list = [1, 2, 3, 4, 5, 2, 9, 7, 0, 10]
 
 list.sort()
 print(list)
 
-#To reverse the list
+# To reverse the list
 list.sort(reverse=True)
 print(list)
 
-# To print last 5 numbers
-print(list[-5:])
-
-# To find the largest number
+#To find Max
 print(max(list))
+
+#To find 5 largest number
+print(list[:-5])
