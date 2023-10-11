925532f2c0ea7386583e5dab122287d38adeb7c3
AJITH D <ajith.d2007@gmail.com>
2023-10-11 11:01:23 +0530

    Fundamentals

--- Practice/Reversing String.py
+++ Practice/Reversing String.py
@@ -1,3 +1,25 @@
 name = 'Ajith Dhanasekaran'
 
 print(name[::-1])
+
+#Reversing string without affecting special characters
+input = 'abc/de*fg^ij'
+
+# Converting into list
+l = list(input)
+
+i = 0
+j = len(l)-1
+
+while i<j:
+    if not l[i].isalpha():
+        i+=1
+    elif not l[j].isalpha():
+        j-=1
+    else:
+        l[i], l[j] = l[j], l[i]
+        i+=1
+        j-=1
+
+out = ''.join(l)
+print(out)
--- Practice/frequent item.py
+++ Practice/frequent item.py
@@ -1,4 +1,3 @@
 l = [1, 2, 3, 2, 4, 5, 6, 7, 8, 1, 2]
 
 print(max(set(l), key=l.count))
-
