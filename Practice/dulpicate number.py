2c5fa9be930b9c8e6211d2052f901ffc634f04f9
AJITH D <ajith.d2007@gmail.com>
2023-10-11 10:18:17 +0530

    Fundamentals

--- Practice/dulpicate number.py
+++ Practice/dulpicate number.py
@@ -1,10 +1,8 @@
 list = [1, 2, 3, 4, 5, 6, 4, 7, 8, 1, 9, 2]
 r = []
 
-for char in list:
-    if char not in r:
-        r.append(char)
+for i in list:
+    if i not in r:
+        r.append(i)
+
 print(r)
-
-num = int(input('Enter: '))
-
