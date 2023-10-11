bcf5da7f752dfd417e1156654f715a0335ebef37
AJITH D <ajith.d2007@gmail.com>
2023-10-11 10:21:31 +0530

    Fundamentals

--- Practice/Factorial.py
+++ Practice/Factorial.py
@@ -1,12 +1,13 @@
 import math
-def fact(num):
-    f = math.factorial(num)
-    return f
-print(fact(6))
+
+def fac(num):
+    product = math.factorial(num)
+    return product
+print(fac(6))
 
 l = [1, 2, 3, 4, 5, 6, 4, 7, 8, 1, 9, 2]
-print(list(map(fact, l)))
+print(list(map(fac, l)))
 
-for i in range(1, 11):
+for i in range(0, 11):
     product = math.factorial(i)
     print(product)
