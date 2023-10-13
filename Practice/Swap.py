a947593d5a895a4a56e2d57e295a288a7a87235e
AJITH D <ajith.d2007@gmail.com>
2023-10-13 09:44:46 +0530

    Fundamentals

--- Practice/Swap.py
+++ Practice/Swap.py
@@ -4,3 +4,13 @@ y = 33
 x, y = y, x
 print('x=', x)
 print('y=', y)
+
+# Swapping first and last elements
+
+l = [1, 2, 3, 4, 5, 6]
+
+l[0], l[-1] = l[-1], l[0]
+print(l)
+
+l[0], l[2] = l[2], l[0]
+print(l)
