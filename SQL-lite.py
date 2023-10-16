dc3357c2bbf334128b98ccf7cb7b3a11416a249e
AJITH D <ajith.d2007@gmail.com>
2023-10-16 08:58:29 +0530

    Fundamentals

--- SQL-lite.py
+++ SQL-lite.py
@@ -15,7 +15,7 @@ class AppForm:
         self.v2 = QVBoxLayout()
         self.title = QLabel()
         self.id = QLabel()
-        self.line = QLineEdit()
+        self.id_line = QLineEdit()
         self.name = QLabel()
         self.name_le = QLineEdit()
         self.age = QLabel()
@@ -38,6 +38,44 @@ class AppForm:
         self.db = self.connection.cursor()
 
     def widget(self):
+        self.title.setText('Registration')
+        self.id.setText('Register No: ')
+        self.name.setText('Name: ')
+        self.age.setText('Age: ')
+        self.join.setText('Joined:')
+        self.complete.setText('Completed: ')
+        self.city.setText('City: ')
+        self.students.setStyleSheet('background-color:#a22d75; color:white')
+        self.pb_register.setText('Register')
+        self.pb_register.setStyleSheet('background-color:#a22d75; color:white')
+        self.pb_update.setText('Update')
+        self.pb_update.setStyleSheet('background-color:#a22d75; color:white')
+        self.pb_delete.setText('Delete')
+        self.pb_delete.setStyleSheet('background-color:#a22d75; color:white')
+        self.pb_new.setText('New')
+        self.pb_new.setStyleSheet('background-color:#a22d75; color:white')
+
+        # Layout
+        self.v1.addWidget(self.title)
+        self.v1.addWidget(self.id)
+        self.v1.addWidget(self.id_line)
+        self.v1.addWidget(self.name)
+        self.v1.addWidget(self.name_le)
+        self.v1.addWidget(self.age)
+        self.v1.addWidget(self.age_le)
+        self.v1.addWidget(self.join)
+        self.v1.addWidget(self.join_le)
+        self.v1.addWidget(self.complete)
+        self.v1.addWidget(self.complete_le)
+        self.v1.addWidget(self.city)
+        self.v1.addWidget(self.city_le)
+        self.v1.addWidget(self.h2)
+        self.v1.addStretch()
+        self.v2.addWidget(self.students)
+
+        self.h1.addLayout(self.v1)
+        self.h1.addLayout(self.v2)
+
 
     def select(self):
 
